433dce318a2e1d710e595e501a6cec444c094a2e
dodi herlangga <37464431+dodichakill@users.noreply.github.com>
2023-09-13 13:04:16 +0700

    UI(about) : change image headline

--- src/app/tentang/page.jsx
+++ src/app/tentang/page.jsx
@@ -1,14 +1,18 @@
-"use client";
-
 import NavigationBar from "@components/NavigationBar";
 import Background from "./Background";
 import Features from "./Features";
 import Headline from "./Headline";
 import Teams from "./Teams";
-export default function Tentang() {
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+export default async function Tentang() {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   return (
     <>
-      <NavigationBar active="" />
+      <NavigationBar active="" user={user} />
       <Headline />
       <Background />
       <Features />
