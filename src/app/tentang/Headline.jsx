023d76ddc9f2b0b4bbdc9ea84752a90da4a074f5
dodi herlangga <37464431+dodichakill@users.noreply.github.com>
2023-09-13 11:54:36 +0700

    UI: slicing about page

---
+++ src/app/tentang/Headline.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Headline() {
+  return (
+    <div
+      className="w-full h-96 md:h-screen flex justify-center items-center mb-10 md:mb-28"
+      style={{
+        backgroundImage: "url('/assets/About/h_about.png')",
+        objectFit: "cover",
+      }}
+    >
+      <div className="px-10 bg-white/50 backdrop-blur-lg text-center py-20 ">
+        <h1 className="heading">Sebuah Perjalanan</h1>
+        <p className="desc">
+          Kenalan lebih dekat dengan sejarah TranquilMind.id dan orang-orang
+          dibaliknya
+        </p>
+      </div>
+    </div>
+  );
+}
