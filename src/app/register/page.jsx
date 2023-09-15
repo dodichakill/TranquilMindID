0cbff44e4eee5093a45a9a0c68ac28214d0cf5ed
dodi herlangga <37464431+dodichakill@users.noreply.github.com>
2023-09-15 19:32:04 +0700

    update: remove supabase

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,13 +1,5 @@
-import { redirect } from "next/navigation";
-
 import RegisterComponent from "./RegisterComponent";
 const Register = async () => {
-  const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.auth.getSession();
-
-  if (data?.session) {
-    redirect("/");
-  }
   return <RegisterComponent />;
 };
 
